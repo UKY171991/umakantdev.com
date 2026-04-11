7a8ee575f7a1c1b2de83c9695ed723dbf8b92d34
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2026-04-11 13:53:50 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
